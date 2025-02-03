2fbdfc616d92b8b62f02930c10b9a38b1c5c38ca
fabio-baraoe <fbarao@sadebr.com.br>
2025-02-03 12:19:59 -0300

    Adicionando script para instalar dependências

--- install_requirements.py
+++ install_requirements.py
@@ -1,16 +1,10 @@
-import os
 import subprocess
 import sys
 
-def instalar_dependencias():
-    print("📦 Instalando dependências do requirements.txt...")
+print("🚀 Instalando dependências do requirements.txt...")
 
-    try:
-        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip"], check=True)
-        subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
-        print("✅ Dependências instaladas com sucesso!")
-    except subprocess.CalledProcessError as e:
-        print(f"❌ Erro ao instalar dependências: {e}")
-
-if __name__ == "__main__":
-    instalar_dependencias()
+try:
+    subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], check=True)
+    print("✅ Instalação concluída com sucesso!")
+except subprocess.CalledProcessError as e:
+    print(f"❌ Erro ao instalar dependências: {e}")
